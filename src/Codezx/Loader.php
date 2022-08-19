b93584770a1dbd5c0de9b81ba7f7f02c0cb8ae4f
SxxCodzx <tbralex7@gmail.com>
2022-08-19 17:55:03 -0500

    Add New Config

--- src/Codezx/Loader.php
+++ src/Codezx/Loader.php
@@ -5,14 +5,15 @@ namespace Codezx;
 use pocketmine\player\Player;
 use pocketmine\Server;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
 
 class Loader extends PluginBase{
 
     public $autofeed = [];
 
     public function onEnabel(): void{
-        $this->prefix = $this->getConfig()->get('prefix');
-        $this->auto = $this->getConfig()->get('autofeed-message');
-        $this->feed = $this->getConfig()->get('feed-message');
+        $this->prefix = $this->getConfig()->get(TextFormat::colorize('prefix'));
+        $this->auto = $this->getConfig()->get(TextFormat::colorize('autofeed-message'));
+        $this->feed = $this->getConfig()->get(TextFormat::colorize('feed-message'));
     }
 }
